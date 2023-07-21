c16a7f68d5645819632c6e7fbdb0b7b727f6d005
Fazle Rabbi <fazlerabbi@Fazles-MacBook-Pro.local>
2023-07-21 19:27:52 +0600

    1st commit

--- api/src/App.vue
+++ api/src/App.vue
@@ -1,35 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-// import test from './components/test.vue'
 import countries from './data/countries'
 
-const link = {
-
-  imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg-3382ZgdUhzsOz0VYE8KVNtX_HTwTxRSps08Nli1&s",
-  alt: "image",
-  linkUrl: "https://ostad-assignment-module3.netlify.app/",
-  
-} 
+function findB(string) {
+  const regex = /[A-Z]/g;
+  return string.match(regex);
+}
 
 </script>
 
 <template>
   <div>
-    <!-- <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
-
-  <!-- <a :href="link.linkUrl"><img :src="link.imageUrl" :alt="link.alt" style="width:500px"></a>
-  <hr>
-   -->
-  
-   <div v-for="country in countries" v-show="'B'==country.country[0]">{{ country.country }}<hr></div>
-
-
-
+    <h2>Countries whose names start with 'B'</h2>
+   <div v-for="country in countries" v-show="'B'==findB(country.country)[0]">{{ country.country }}<hr></div>
   </div>
  
 </template>
